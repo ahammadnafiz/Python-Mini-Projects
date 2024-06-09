aadd8b781a1579ccf47b72ef31a9dbeb5f6e673c
ahammadnafiz <86776685+ahammadnafiz@users.noreply.github.com>
2024-06-09 22:20:30 +0600

    remove search

--- Oratio/voice_assistant.py
+++ Oratio/voice_assistant.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
 import logging
-import tkinter as tk
-import customtkinter as ctk
 import speech_recognition as sr
 import pyttsx3
 from langchain.chains import LLMChain
