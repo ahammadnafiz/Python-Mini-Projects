6be8c2515387d0d518b0af422b6cd311460d2a2e
ahammadnafiz <86776685+ahammadnafiz@users.noreply.github.com>
2024-06-11 22:25:31 +0600

    remove error msg

--- Oratio/voice_assistant.py
+++ Oratio/voice_assistant.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import logging
 import speech_recognition as sr
 import pyttsx3
-from langchain.chains import LLMChain
+from langchain.chains import ConversationChain
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain_core.messages import SystemMessage
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
@@ -17,10 +17,8 @@ logging.basicConfig(level=logging.INFO)
 logging.getLogger('comtypes').setLevel(logging.WARNING)
 
 load_dotenv('.env')
-os.environ["GOOGLE_API_KEY"] = os.getenv('API_KEY')
 
 engine = pyttsx3.init()
-
 
 class VoiceAssistant:
     def __init__(self, groq_api_key):
@@ -28,11 +26,12 @@ class VoiceAssistant:
         self.memory = ConversationBufferWindowMemory(k=5, memory_key="chat_history", return_messages=True)
         self.groq_chat = ChatGroq(groq_api_key=self.groq_api_key, model_name='llama3-8b-8192')
         self.prompt = self._create_chat_prompt()
-        self.conversation = LLMChain(
+        self.conversation = ConversationChain(
             llm=self.groq_chat,
             prompt=self.prompt,
             verbose=True,
             memory=self.memory,
+            input_key="human_input",  # Add this line
         )
 
     def answer(self, prompt):
@@ -87,25 +86,29 @@ class VoiceAssistant:
         Returns:
             ChatPromptTemplate: The chat prompt template.
         """
+        system_message = SystemMessage(
+            content='''
+            You are a witty, insightful assistant that can Analyze and provide insights across diverse topics. 
+            When answering questions, aim to provide responses that are a bit more 
+            substantial—about 1 to 3 sentences long. This allows you to offer more 
+            context, share interesting observations, or add a touch of personality.
+            Keep your tone friendly, slightly witty, and conversational. Imagine you're 
+            a perceptive friend who's good at listening situations. Feel free to make 
+            educated guesses about what you listen, but phrase them as observations, not 
+            facts.
+            '''
+        )
+
+        human_message_prompt = HumanMessagePromptTemplate.from_template("{human_input}")
+
         prompt = ChatPromptTemplate.from_messages(
             [
-                SystemMessage(content=
-                              '''
-                            You are a witty, insightful assistant that can Analyze and provide insights across diverse topics. 
-                            When answering questions, aim to provide responses that are a bit more 
-                            substantial—about 1 to 3 sentences long. This allows you to offer more 
-                            context, share interesting observations, or add a touch of personality.
-                            Keep your tone friendly, slightly witty, and conversational. Imagine you're 
-                            a perceptive friend who's good at listening situations. Feel free to make 
-                            educated guesses about what you listen, but phrase them as observations, not 
-                            facts.
-                              '''),
+                system_message,
                 MessagesPlaceholder(variable_name="chat_history"),
-                HumanMessagePromptTemplate.from_template("{human_input}"),
+                human_message_prompt,
             ]
         )
         return prompt
-
 
 def audio_callback(recognizer, audio, assistant):
     """
@@ -127,7 +130,6 @@ def audio_callback(recognizer, audio, assistant):
     except Exception as e:
         print("Error during audio recognition:", e)
 
-
 def handle_voice_assistant():
     """
     Handles voice input and passes it to the voice assistant.
@@ -137,7 +139,6 @@ def handle_voice_assistant():
         print("Listening...")
         audio = recognizer.listen(source)
     audio_callback(recognizer, audio, voice_assistant)
-
 
 def run_async(func):
     """
@@ -155,14 +156,12 @@ def run_async(func):
         return thread
     return wrapper
 
-
 @run_async
 def process_voice_assistant():
     """
     Process voice assistant asynchronously.
     """
     handle_voice_assistant()
-
 
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 
