c9944d999ec97669a0a2a649ba7109c452593f52
ahammadnafiz <86776685+ahammadnafiz@users.noreply.github.com>
2024-07-25 00:51:43 +0600

    Rokomari Analysis

--- Rokomari Book Prediction/bookdetails_scrapher.py
+++ Rokomari Book Prediction/bookdetails_scrapher.py
@@ -8,12 +8,12 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 def fetch_html(url):
     response = requests.get(url)
-    if response.status_code == 200:
+    if response.status_code == 200: 
         return response.content
     else:
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
         return None
-
+ 
 def scrape_page(url):
     html_content = fetch_html(url)
     if html_content:
