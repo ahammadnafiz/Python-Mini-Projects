a4d388314302877a2abfce81bb9f2d0c41d7eb16
ahammadnafiz <ahammadnafiz@outlook.com>
2025-02-23 08:10:54 +0600

    Remove obsolete binary files and update setup.py for cleanup

--- RepoRAG/setup.py
+++ RepoRAG/setup.py
@@ -1,16 +1,1 @@
-# setup.py
-from setuptools import setup, find_packages
-
-setup(
-    name='repoprompter',
-    version='0.1.0',
-    packages=find_packages(),
-    install_requires=[
-        'PyGithub',
-    ],
-    entry_points={
-        'console_scripts': [
-            'repoprompter=repoprompter.repoprompter.main:main',
-        ],
-    },
-)
+#TODO
